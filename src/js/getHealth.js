b032a1dae69c599a2f6072ae1e9bb33daee436c5
Vl-pod <110475023+Vl-pod@users.noreply.github.com>
2023-08-24 16:33:10 +0400

    create and add __tests__

--- src/js/getHealth.js
+++
@@ -1,9 +0,0 @@
-export default function getHealthStatus(character) {
-  if (character.health > 50) {
-    return 'healthy';
-  } if (character.health <= 50 && character.health >= 15) {
-    return 'wounded';
-  }
-  return 'critical';
-}
-export const character = { name: 'Маг', health: 90 };
